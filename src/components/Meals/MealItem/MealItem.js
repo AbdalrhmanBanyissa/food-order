8e09c12690f7f7958dfb36b7574eb39ea413216b
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2022-09-21 15:35:28 +0300

    working on MealItem component

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  return "";
+};
+
+export default MealItem;
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./MealItem.module.css";
+
+const MealItemForm = (props) => {
+  return "";
+};
+
+export default MealItemForm;
